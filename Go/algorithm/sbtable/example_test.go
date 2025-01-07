6cd5d40e889c311432a35573bd12dd27556531d2
An Ha <habinhan@proton.me>
2025-01-07 21:21:57 +0700

    symbol table example

---
+++ Go/algorithm/sbtable/example_test.go
@@ -0,0 +1,34 @@
+package sbtable_test
+
+import (
+	"fmt"
+	"github.com/bannnn511/code/go/algorithm/sbtable"
+)
+
+func ExampleBinarySearchST() {
+	// Create a new Binary Search Symbol Table
+	bst := sbtable.NewBST[string, int]()
+
+	// Insert key-value pairs
+	bst.Put("a", 1)
+	bst.Put("b", 2)
+	bst.Put("c", 3)
+
+	// Retrieve values
+	fmt.Println(bst.Get("a"))
+	fmt.Println(bst.Get("b"))
+	fmt.Println(bst.Get("c"))
+
+	// Check the rank of keys
+	fmt.Println(bst.Rank("a"))
+	fmt.Println(bst.Rank("b"))
+	fmt.Println(bst.Rank("c"))
+
+	// Output:
+	// 1
+	// 2
+	// 3
+	// 2
+	// 1
+	// 0
+}
