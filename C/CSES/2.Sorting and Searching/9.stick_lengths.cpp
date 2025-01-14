b228b9620bffaf10644e45b3f32e54be9ecc5b61
An Ha <habinhan@proton.me>
2025-01-14 17:00:18 +0700

    edit distance method

--- C/CSES/2.Sorting and Searching/9.stick_lengths.cpp
+++ C/CSES/2.Sorting and Searching/9.stick_lengths.cpp
@@ -32,3 +32,34 @@ int main() {
 
     return 0;
 }
+
+
+// int main() {
+//     int n;
+//     cin >> n;
+//
+//     int a[maxN] = {};
+//     for (int i = 0; i < n; i++) {
+//         cin >> a[i];
+//     }
+//
+//     ll minValue = numeric_limits<ll>::max() - 1;
+//     ll ready[maxN] = {};
+//     fill_n(ready, maxN, -1);
+//
+//     for (int i = 0; i < n; i++) {
+//         ll sum = 0;
+//         int j;
+//         for (j = 0; j < n; j++) {
+//             if (ready[j] != -1) {
+//                 sum = ready[j];
+//                 break;
+//             }
+//             sum += abs(a[i] - a[j]);
+//         }
+//         ready[j] = sum;
+//         minValue = min(sum, minValue);
+//     }
+//     cout << minValue;
+//
+//     return 0;
