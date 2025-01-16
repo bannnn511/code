67e9c652e5e23d0c0c0fa806924601d2a4edd530
An Ha <habinhan@proton.me>
2025-01-16 16:09:44 +0700

    grid paths

---
+++ Go/algorithm/CSES/2.dynamic_programming/6.grid_paths.go
@@ -0,0 +1,26 @@
+package __dynamic_programming
+
+func gridPath(squares [][]int) int {
+	length := len(squares)
+
+	paths := make([][]int, length)
+	for i := range length {
+		paths[i] = make([]int, length)
+	}
+
+	for x := 1; x < length; x++ {
+		for y := 1; y < length; y++ {
+			if x == 1 && y == 1 {
+				paths[x][y] = 1
+				continue
+			}
+			if squares[x][y] == -1 {
+				paths[x][y] = 0
+			} else {
+				paths[x][y] = paths[x-1][y] + paths[x][y-1]
+			}
+		}
+	}
+
+	return paths[length-1][length-1]
+}
---
+++ Go/algorithm/CSES/2.dynamic_programming/6.grid_paths_test.go
@@ -0,0 +1,67 @@
+package __dynamic_programming
+
+import "testing"
+
+func Test_gridPath(t *testing.T) {
+	type args struct {
+		squares [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "case 1",
+			args: args{
+				squares: [][]int{
+					{0, 0, 0},
+					{0, 0, 0},
+					{0, 0, 0},
+				},
+			},
+			want: 2,
+		},
+		{
+			name: "case 2",
+			args: args{
+				squares: [][]int{
+					{0, 0, 0},
+					{0, 0, -1},
+					{0, 0, 0},
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "case 3",
+			args: args{
+				squares: [][]int{
+					{0, 0, 0},
+					{0, 0, -1},
+					{0, -1, 0},
+				},
+			},
+			want: 0,
+		},
+		{
+			name: "case 4",
+			args: args{
+				squares: [][]int{
+					{0, 0, 0, 0, 0},
+					{0, 0, 0, 0, 0},
+					{0, 0, -1, 0, 0},
+					{0, 0, 0, 0, -1},
+					{0, -1, 0, 0, 0},
+				}},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := gridPath(tt.args.squares); got != tt.want {
+				t.Errorf("gridPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
