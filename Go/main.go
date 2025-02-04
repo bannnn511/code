21e91f5a667f6aab03611478d4acaae43a272d14
An Ha <habinhan@proton.me>
2025-02-04 17:11:45 +0700

    testing go async preempt

--- Go/main.go
+++ Go/main.go
@@ -1,9 +1,51 @@
 package main
 
 import (
-	"github.com/bannnn511/code/go/algorithm/examples/LRU"
+	"fmt"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+	"time"
 )
 
+var pid int
+
+const loop = 1e9
+
+func stingy(money *int) {
+	for i := 0; i < loop; i++ {
+		*money += 10
+		// syscall.Kill(pid, syscall.SIGURG)
+	}
+	fmt.Println("Stingy Done")
+}
+func spendy(money *int) {
+	for i := 0; i < loop; i++ {
+		*money -= 10
+		// syscall.Kill(pid, syscall.SIGURG)
+	}
+	fmt.Println("Spendy Done")
+}
+
 func main() {
-	LRU.CacheLRU()
+	pid = syscall.Getpid()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGURG)
+	go func() {
+		for {
+			select {
+			case <-c:
+				fmt.Println("signal -----", pid)
+			default:
+			}
+		}
+	}()
+
+	runtime.GOMAXPROCS(1)
+	money := 100
+	go spendy(&money)
+	go stingy(&money)
+	time.Sleep(1 * time.Second)
+	println("Money in bank account: ", money)
 }
