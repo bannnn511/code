d17b103919239ce9aad2928920aa5f7e50e1e1a1
An Ha <habinhan@proton.me>
2025-02-13 17:49:35 +0700

    reuseable barrier

--- C/ostep/semaphore/resuable_barrier.c
+++ C/ostep/semaphore/resuable_barrier.c
@@ -17,7 +17,8 @@ typedef struct __barrier_t
     int n; // number of threads
     int count;
     zem_t mutex;
-    zem_t barrier;
+    zem_t turnstile1;
+    zem_t turnstile2;
 } barrier_t;
 
 // the single barrier we are using for this program
@@ -28,20 +29,39 @@ void barrier_init(barrier_t *b, int num_threads)
     b->n = num_threads;
     b->count = 0;
     zem_init(&b->mutex, 1);
-    zem_init(&b->barrier, 0);
+    zem_init(&b->turnstile1, 0);
+    zem_init(&b->turnstile2, 1);
 }
 
-void barrier(barrier_t *b)
+void barrier(barrier_t *b, int thread_id)
 {
     zem_wait(&b->mutex);
     b->count++;
-    zem_post(&b->mutex);
     if (b->count == b->n)
     {
-        zem_post(&b->barrier);
+        zem_wait(&b->turnstile2);
+        zem_post(&b->turnstile1);
     }
-    zem_wait(&b->barrier);
-    zem_post(&b->barrier);
+    zem_post(&b->mutex);
+
+    zem_wait(&b->turnstile1);
+    zem_post(&b->turnstile1);
+
+    printf("Thread %d critical point\n", thread_id);
+
+    zem_wait(&b->mutex);
+    b->count--;
+    if (b->count == 0)
+    {
+        zem_wait(&b->turnstile1);
+        zem_post(&b->turnstile2);
+    }
+    zem_post(&b->mutex);
+
+    zem_wait(&b->turnstile2);
+    zem_post(&b->turnstile2);
+
+    printf("Thread %d exit\n", thread_id);
 }
 
 //
@@ -50,41 +70,52 @@ void barrier(barrier_t *b)
 typedef struct __tinfo_t
 {
     int thread_id;
+    int num_barriers;
 } tinfo_t;
 
 void *child(void *arg)
 {
     tinfo_t *t = (tinfo_t *)arg;
-    printf("child %d: before\n", t->thread_id);
-    barrier(&b);
-    printf("child %d: after\n", t->thread_id);
+    for (int i = 0; i < t->num_barriers; i++)
+    {
+        printf("child %d: before\n", t->thread_id);
+        barrier(&b, t->thread_id);
+        sleep(1);
+        printf("child %d: after\n", t->thread_id);
+    }
     return NULL;
 }
 
-// run with a single argument indicating the number of
-// threads you wish to create (1 or more)
+// Modified main function
 int main(int argc, char *argv[])
 {
-    assert(argc == 2);
+    assert(argc == 3);
     int num_threads = atoi(argv[1]);
+    int num_barriers = atoi(argv[2]);
     assert(num_threads > 0);
+    assert(num_barriers > 0);
 
     pthread_t p[num_threads];
     tinfo_t t[num_threads];
 
-    printf("parent: begin\n");
     barrier_init(&b, num_threads);
 
-    int i;
-    for (i = 0; i < num_threads; i++)
+    // Create threads
+
+    printf("parent: begin\n");
+    for (int i = 0; i < num_threads; i++)
     {
         t[i].thread_id = i;
+        t[i].num_barriers = num_barriers;
         Pthread_create(&p[i], NULL, child, &t[i]);
     }
 
-    for (i = 0; i < num_threads; i++)
+    // Wait for all threads to complete
+    for (int i = 0; i < num_threads; i++)
+    {
         Pthread_join(p[i], NULL);
-
+    }
     printf("parent: end\n");
+
     return 0;
 }
--- C/ostep/semaphore/zemaphore.c
+++ C/ostep/semaphore/zemaphore.c
@@ -8,19 +8,22 @@
  * Semaphore implementation since MacOS does not support unnamed semaphore.
  */
 
-typedef struct zemaphore_ {
+typedef struct zemaphore_
+{
   int value;
   pthread_cond_t cond;
   pthread_mutex_t lock;
 } zem_t;
 
-void zem_init(zem_t *z, int value) {
+void zem_init(zem_t *z, int value)
+{
   z->value = value;
   Cond_init(&z->cond);
   Mutex_init(&z->lock);
 }
 
-int zem_post(zem_t *z) {
+int zem_post(zem_t *z)
+{
   Mutex_lock(&z->lock);
   const int tmp = z->value++;
   Cond_signal(&z->cond);
@@ -29,11 +32,14 @@ int zem_post(zem_t *z) {
   return tmp;
 }
 
-int zem_wait(zem_t *z) {
+int zem_wait(zem_t *z)
+{
   Mutex_lock(&z->lock);
-  while (z->value <= 0) {
+  while (z->value <= 0)
+  {
     Cond_wait(&z->cond, &z->lock);
   }
+  z->value--;
   Mutex_unlock(&z->lock);
   return 0;
 }
