8b8583c13a8bbfd2f171fff075054c4b002bdb73
An Ha <habinhan@proton.me>
2025-04-02 18:49:09 +0700

    recv send loop

--- C/unix/socket/simple_server_client/server.c
+++ C/unix/socket/simple_server_client/server.c
@@ -7,10 +7,19 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <time.h>
 
 
 #define MYPORT "8000"
 #define BACKLOG 20
+#define INTERVAL 2      // seconds between requests
+
+volatile sig_atomic_t keep_running = 1;
+
+void sigint_handler(int sig) {
+    keep_running = 0;
+}
+
 
 void sigchld_handler(int s) {
     // waitpid() might overwrite errno, so we save and restore it:
@@ -28,13 +37,6 @@ void *get_in_addr(struct sockaddr *sa) {
 }
 
 int main() {
-    // getaddrinfo
-    // socket
-    // bind
-    // listen
-    // accept
-    // send
-
     struct addrinfo hints, *res, *p;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
@@ -48,13 +50,21 @@ int main() {
     }
 
 
-    int socket_fd;
+    int socket_fd = -1;
     for (p = res; p != NULL; p = p->ai_next) {
         socket_fd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
         if (socket_fd == -1) {
             perror("server: socket");
             continue;
         }
+
+        int yes = 1;
+        if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+            perror("setsockopt");
+            close(socket_fd);
+            continue;
+        }
+
         // make a socket, bind
         if (bind(socket_fd, p->ai_addr, p->ai_addrlen) == -1) {
             close(socket_fd);
@@ -89,8 +99,9 @@ int main() {
     printf("listening on %s port %s\n", hostname, MYPORT);
 
     // reap child processes
-    struct sigaction sa;
+    struct sigaction sa, sa_cancel;
     sigemptyset(&sa.sa_mask);
+    sigemptyset(&sa_cancel.sa_mask);
     sa.sa_flags = 0;
     sa.sa_handler = sigchld_handler;
     if (sigaction(SIGCHLD, &sa, NULL) == -1) {
@@ -98,27 +109,28 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
+    sa_cancel.sa_flags = 0;
+    sa_cancel.sa_handler = sigint_handler;
+    if (sigaction(SIGINT, &sa_cancel, NULL) == -1) {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
+
     const char *msg = "Hello, World";
-    size_t bytes_sent;
     size_t len = strlen(msg);
     int count = 0;
-    while (1) {
+    while (keep_running) {
         // accept incoming connection
         struct sockaddr_storage their_addr;
         socklen_t addr_size = sizeof(their_addr);
         char buf[INET6_ADDRSTRLEN];
         int newfd = accept(socket_fd, (struct sockaddr *) &their_addr, &addr_size);
         if (newfd == -1) {
-            perror("accept");
+            // perror("accept");
             continue;
         }
         inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *) &their_addr), buf, sizeof(buf));
         printf("connection from: %s\n", buf);
-
-        if (count > 5) {
-            exit(EXIT_SUCCESS);
-        }
-
 
         pid_t pid;
         switch (pid = fork()) {
@@ -127,16 +139,35 @@ int main() {
                 exit(EXIT_FAILURE);
             case 0:
                 close(socket_fd);
-                bytes_sent = send(newfd, msg, len, 0);
-                if (bytes_sent == -1) {
-                    perror("server: send");
+                pid_t child = getpid();
+                while (keep_running) {
+                    char buffer[1024];
+                    ssize_t number_bytes = recv(newfd, buffer, sizeof(buf), 0);
+                    if (number_bytes == 0) {
+                        printf("server: client disconnect on thread: %d\n", child);
+                        break;
+                    }
+                    if (number_bytes == -1) {
+                        fprintf(stderr, "client: failed to recv\n");
+                        exit(EXIT_FAILURE);
+                    }
+
+                    size_t bytes_sent = send(newfd, msg, len, 0);
+                    printf("server: send on thread: %d\n", child);
+                    if (bytes_sent == -1) {
+                        perror("server: send");
+                    }
+
+                    printf("bytes sent: %ld\n", bytes_sent);
+                    sleep(INTERVAL);
                 }
-                printf("bytes sent: %ld\n", bytes_sent);
                 close(newfd);
                 exit(0);
+
             default:
                 count++;
         }
+        // parent close
         close(newfd);
     }
 
