c3b3c2e3989ee1632067e781b918bd2058815625
An Ha <habinhan@proton.me>
2025-06-07 18:52:15 +0700

    fix handle_message

--- C/projects/reliable_udp/client.c
+++ C/projects/reliable_udp/client.c
@@ -1,42 +1,33 @@
 #include "io.c"
-#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #define BUFFSIZE 500
 
-int main(int argc, char **argv)
-{
-  char *host;
-  char *message;
-  int port;
-  int clientfd;
+/**
+ * Usage: client <host> <port> <message>
+ */
+int main(int argc, char **argv) {
+    if (argc != 4) {
+        fprintf(stderr, "Usage: %s <host> <port> <message>\n", argv[0]);
+        exit(1);
+    }
 
+    const char *host = argv[1];
+    const int port = atoi(argv[2]);
+    if (port <= 0 || port > 65535) {
+        fprintf(stderr, "Invalid port number: %d\n", port);
+        exit(1);
+    }
 
-  if (argc != 4)
-  {
-    fprintf(stderr, "Usage: %s <host> <port> <message>\n", argv[0]);
-    exit(1);
-  }
+    char *message = argv[3];
+    char res[BUFFSIZE];
 
-  host = argv[1];
-  port = atoi(argv[2]);
-  if (port <= 0 || port > 65535)
-  {
-    fprintf(stderr, "Invalid port number: %d\n", port);
-    exit(1);
-  }
+    int status = make_request(host, argv[2], message, res);
+    if (status == -1) {
+        perror("make request");
+        exit(EXIT_FAILURE);
+    }
 
-  message = argv[3];
-  char res[BUFFSIZE];
-
-  int status = make_request(host, argv[2], message, res);
-  if (status < -1)
-  {
-    perror("make request");
-    exit(EXIT_FAILURE);
-  }
-
-  return 0;
+    return 0;
 }
--- C/projects/reliable_udp/server.c
+++ C/projects/reliable_udp/server.c
@@ -3,36 +3,36 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 
-void handle_message(int socket_fd, struct sockaddr *addr, char *req) {
-  char reply[] = "handling request";
-  int size = strlen(reply);
-  printf("Handling message: %s\n", req);
+void *handle_message(int socket_fd, struct sockaddr *addr, char *req) {
+    char reply[] = "handling request";
+    int size = strlen(reply);
+    printf("Handling message: %s\n", req);
 
-  UDP_Write(socket_fd, addr, reply, size);
+    UDP_Write(socket_fd, addr, reply, size);
+    return NULL;
 }
 
+/**
+ * Usage: server <port>
+ */
 int main(int argc, char **argv) {
-  char *host;
-  int port;
-  int clientfd;
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
 
-  if (argc != 2) {
-    fprintf(stderr, "Usage: %s <port>\n", argv[0]);
-    exit(1);
-  }
+    int port = atoi(argv[1]);
+    if (port <= 0 || port > 65535) {
+        fprintf(stderr, "Invalid port number: %d\n", port);
+        exit(1);
+    }
 
-  port = atoi(argv[1]);
-  if (port <= 0 || port > 65535) {
-    fprintf(stderr, "Invalid port number: %d\n", port);
-    exit(1);
-  }
+    const int socket_fd = udp_create_server(argv[1]);
+    if (socket_fd == -1) {
+        exit(EXIT_FAILURE);
+    }
 
-  int socket_fd = udp_create_server(argv[1]);
-  if (socket_fd == -1) {
-    exit(EXIT_FAILURE);
-  }
+    udp_request_handler(socket_fd, handle_message);
 
-  udp_request_handler(socket_fd, (void*)handle_message);
-
-  return 0;
+    return 0;
 }
