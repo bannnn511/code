659f0b358f869e0a155e5bdfc1b00e35799e9a5a
An Ha <habinhan@proton.me>
2025-07-01 21:09:51 +0700

    strings

--- C/books/knn_c_programming_mordern_approach/14_strings/exercise.c
+++ C/books/knn_c_programming_mordern_approach/14_strings/exercise.c
@@ -4,16 +4,38 @@
 
 #define BUFFER_SIZE 100
 
-void capitalize(char *buffer, int n) {
-  for (int i = 0; i < n; i++) {
-    buffer[i] = toupper(buffer[i]);
+void capitalize(char buffer[]) {
+  int i = 0;
+  while (buffer[i] != '\0') {
+    if (isalpha(buffer[i]))
+      buffer[i] = toupper(buffer[i]);
+
+    i++;
+  }
+}
+
+void capitalize1(char *buffer) {
+  char *p;
+  for (p = &buffer[0]; *p != '\0'; p++) {
+    *p = toupper(*p);
+  }
+}
+
+void censor(char str[]) {
+  int i = 0;
+  while (str[i + 2] != '\0') {
+    if (strncmp(&str[i], "foo", 3) == 0) {
+      strncpy(&str[i], "xxx", 3);
+    }
+    i++;
   }
 }
 
 int read_line(char *buffer, int size) {
   int ch, i = 0;
 
-  while ((ch = getchar()) != EOF && isspace(ch) && ch != '\n');
+  while ((ch = getchar()) != EOF && isspace(ch) && ch != '\n')
+    ;
 
   if (ch == EOF || ch == '\n') {
     buffer[0] = '\0';
@@ -49,8 +71,29 @@ int main(const int argc, const char *argv[]) {
   printf("string has %d spaces\n", spaces);
 
   char hello[] = "hello";
-  capitalize(hello, strlen(hello));
-  printf("%s", hello);
+  capitalize(hello);
+  printf("%s\n", hello);
+
+  char world[] = "world";
+  capitalize1(world);
+  printf("%s\n", world);
+
+  char foo1[] = "food fool";
+  censor(foo1);
+  printf("censor foo1 %s\n", foo1);
+
+  char *str1;
+  char *str2;
+  strcpy(str1, "computer");
+  strcpy(str2, "science");
+
+  if (strcmp(str1, str2) < 0) {
+    strcat(str1, str2);
+  } else {
+    strcat(str2, str1);
+  }
+  str1[strlen(str1) - 6] = '\0';
+  printf("%s\n", str1);
 
   return 0;
 }
